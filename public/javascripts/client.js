8edea9aa7a3a18ecc0b4a1eb68ce12ec26266096
Xavier Glab <ksaweryglab@gmail.com>
2014-03-22 23:37:49 +0000

    added handlebars and views

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,3 +7,32 @@ var BookCollection = Backbone.Collection.extend({
 	model: Book,
 	url: "/books"
 });
+
+var BookView = Backbone.View.extend({
+	template: "li",
+	className: "book",
+	render: function(){
+		var template = $("#booktemplate").html();
+		var compiled = Handlebars.compile(template);
+		var html = compiled(this.model.attributes);
+		this.$el.html(html);
+		return this;
+
+		//this.el --> is a reference to the view, when its rendered, as HTML object 
+		//this.$el --> same, but now it's wrapped in jQuery object, 
+		//we can use them interchangeably, but every time we want to use jQuery method with
+		// our view, we need to use $el
+	}
+});
+
+var BookCollectionView = Backbone.View.extend({
+	tagName: "ul",
+	className: "books",
+	render: function(){
+		this.collection.each(function(book){
+			var bookView = new BookView({model: book});
+			this.$el.append(bookView.render().el);
+		}, this);
+	  return this;
+	}
+});
--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,1 +1,10 @@
 //kickoff the application
+$(function(){
+	var collection = new BookCollection();
+	collection.fetch({
+		success: function(data){
+			var view = new BookCollectionView({collection: data});
+			$("body").append(view.render().el);
+		}
+	});
+});
